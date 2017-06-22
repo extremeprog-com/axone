2bfad09017230bb32f08250a194b9a47c2017525
trean <trean.mi@gmail.com>
2017-06-22 19:51:14 +0300

    restored pause of audio

--- js/Axone_Voice.js
+++ js/Axone_Voice.js
@@ -4,11 +4,11 @@ var Axone_Voice = {
     , playVoice: function() {
         var event = CatchEvent(Axone_PhraseInited, Axone_PhraseChanged, Axone_PhraseUpdated);
             if( this.currentAudioTrack ) {
-                //var ctrack = this.currentAudioTrack;
-                //setTimeout(function() {
-                //    ctrack.pause();
-                //}, 250);
-                speechSynthesis.cancel();
+                var ctrack = this.currentAudioTrack;
+                setTimeout(function() {
+                   ctrack.pause();
+                }, 250);
+                // speechSynthesis.cancel();
             }
 
             this.currentAudioTrack = new Audio('http://voice.mindboost.me/voice2.mindboost.me/voice/translate_tts?ie=UTF-8&total=1&idx=0&client=t&pitch=-105&prev=input&q=' + event.phrase + '&tl=' + (window.LANG == 'RU'?'ru':'en'));
