21bdcbc0af3bfe69c4e98e82ada80c3bb581c98d
trean <trean.mi@gmail.com>
2017-06-26 21:09:59 +0300

    pause behavior fix, voice fix

--- js/Axone_Controls.js
+++ js/Axone_Controls.js
@@ -18,12 +18,17 @@ var Axone_Controls = {
         $('#play').click(function() {
             if( _this._pauseState ) {
                 $(".ball").css("display", "inline-block");
+                $(".ball-wrapper").css("display", "block");
+                $("#phrased").css("display", "block");
                 _this._exerciseState.go('Playing');
                 FireEvent(new Axone_Controls_PlayClick());
             } else {
                 $(".ball").css("display", "none");
+                $(".ball-wrapper").css("display", "none");
+                $("#phrased").css("display", "none");
                 _this._exerciseState.go('Paused');
                 FireEvent(new Axone_Controls_PauseClick());
+                Axone_Voice.currentAudioTrack.pause();
             }
             _this._pauseState = !_this._pauseState;
         });
--- js/Axone_Voice.js
+++ js/Axone_Voice.js
@@ -11,7 +11,10 @@ var Axone_Voice = {
                 // speechSynthesis.cancel();
             }
 
-            this.currentAudioTrack = new Audio('http://voice.mindboost.me/voice2.mindboost.me/voice/translate_tts?ie=UTF-8&total=1&idx=0&client=t&pitch=-105&prev=input&q=' + event.phrase + '&tl=' + (window.LANG == 'RU'?'ru':'en'));
+            var regex = /(<span class="small">)?([A-Za-z0-9\s\n\t:,.-]*)(<\/span>)?/;
+            var phrase = event.phrase.match(regex)[2];
+            this.currentAudioTrack = new Audio('http://voice.mindboost.me/voice2.mindboost.me/voice/translate_tts?ie=UTF-8&total=1&idx=0&client=t&pitch=-105&prev=input&ttsspeed=0&q=' + phrase + '&tl=' + (window.LANG == 'RU'?'ru':'en'));
+            console.log(this.currentAudioTrack);
             this.currentAudioTrack.play();
         // this.currentAudioTrack = new SpeechSynthesisUtterance(event.phrase);
         // this.currentAudioTrack.lang = LANG.toLowerCase();
