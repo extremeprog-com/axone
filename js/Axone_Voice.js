7bcc1ea63c62f922a56be9e0bf2026e6c2bef739
trean <trean.mi@gmail.com>
2017-07-02 00:08:26 +0300

    mutes on param audio=off

--- js/Axone_Voice.js
+++ js/Axone_Voice.js
@@ -11,14 +11,29 @@ var Axone_Voice = {
                 // speechSynthesis.cancel();
             }
 
+        if (!this.mute()) {
             var regex = /(<span class="small">)?([A-Za-z0-9\s\n\t":,.-;]*)(<\/span>)?/;
             var commaRegex = /,/g;
             var phrase = event.phrase.match(regex)[2].replace(commaRegex, ";");
             this.currentAudioTrack = new Audio('http://voice.mindboost.me/voice2.mindboost.me/voice/translate_tts?ie=UTF-8&q=' + phrase + '&total=1&idx=0&tk=854918.741845&client=t&ttsspeed=0.24&tl=' + (window.LANG == 'RU'?'ru':'en'));
-            console.log(this.currentAudioTrack);
+
             this.currentAudioTrack.play();
+        }
+
         // this.currentAudioTrack = new SpeechSynthesisUtterance(event.phrase);
         // this.currentAudioTrack.lang = LANG.toLowerCase();
         // speechSynthesis.speak(this.currentAudioTrack);
+    }
+    , mute: function() {
+      var obj = {};
+        window.location.search.substr(1).split("&").forEach(function (x) {
+        var regex = /([\w\d]*)(=)([\w\d]*)/i;
+        var matched = x.match(regex);
+        if(matched) {
+          obj[matched[1]] = matched[3];
+        }
+      });
+      if (obj["audio"] && obj["audio"] === "off") return true;
+      return false;
     }
 };
